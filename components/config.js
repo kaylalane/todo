6447489d2d3be2678f6cfa289c959249862fff0a
Kayla Lane <kaylalane@ufl.edu>
2023-06-20 02:39:02 -0400

    Update config.js

--- components/config.js
+++ components/config.js
@@ -16,17 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-let user;
-let key = null;
-export function setUser() {
-  user = auth.currentUser;
-}
-
-
-export function getUser() {
-  return user;
-}
